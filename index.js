fd0882019ab929cabb9119331df1944e5235fa37
Steve Larsen <slarsen-3@att.net>
2021-05-01 20:04:17 -0500

    retrive single piece of data

--- index.js
+++ index.js
@@ -11,11 +11,41 @@ router.get("/", function (req, res, next) {
   pieRepo.get(
     function (data) {
       res.status(200).json({
-        "status": 200,
-        "statusText": "OK",
-        "message": "All pies retrieved.",
-        "data": data,
+        status: 200,
+        statusText: "OK",
+        message: "All pies retrieved.",
+        data: data,
       });
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+// Create GET/id to return a single pie
+router.get("/:id", function (req, res, next) {
+  pieRepo.getById(
+    req.params.id,
+    function (data) {
+      if (data) {
+        res.status(200).json({
+          status: 200,
+          statusText: "OK",
+          message: "Single pie retrieved.",
+          data: data,
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          statusText: "Not Found",
+          message: "The pie '" + req.params.id + "' could not be found.",
+          error: {
+            code: "NOT_FOUND",
+            message: "The pie '" + req.params.id + "' could not be found.",
+          },
+        });
+      }
     },
     function (err) {
       next(err);
--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -1,5 +1,5 @@
 let fs = require("fs");
-const FILE_NAME = "./assets/pies.json"
+const FILE_NAME = "./assets/pies.json";
 let pieRepo = {
   get: function (resolve, reject) {
     fs.readFile(FILE_NAME, function (err, data) {
@@ -7,6 +7,16 @@ let pieRepo = {
         reject(err);
       } else {
         resolve(JSON.parse(data));
+      }
+    });
+  },
+  getById: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        let pie = JSON.parse(data).find((p) => p.id == id);
+        resolve(pie);
       }
     });
   },
